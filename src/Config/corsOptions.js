ac032f7cc17ca482a15c6e6266a7f270111d6e2e
Ankesh Kumar <60851047+notelyfe@users.noreply.github.com>
2025-08-03 20:32:34 +0530

    Update corsOptions.js

--- src/Config/corsOptions.js
+++ src/Config/corsOptions.js
@@ -1,5 +1,5 @@
 // const whitelist = ['https://gossip.notelyfe.me']
-const whitelist = ['https://gossip.notelyfe.me', 'https://notelyfe.me']
+const whitelist = ['https://gossip.notelyfe.live', 'https://notelyfe.live']
 
 // const whitelist = ['http://localhost:3000']
 
